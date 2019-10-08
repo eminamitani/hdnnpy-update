6bb9be25dc3a9a2928a1d296cadd2574b72fad92
Emi <emi0716@gmail.com>
2019-10-08 22:18:50 +0900

    try BFGS

--- hdnnpy/aseInterface/test/test.py
+++ hdnnpy/aseInterface/test/test.py
@@ -3,15 +3,17 @@ import pathlib
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 from aseInterface import *
-from ase.build import bulk
+from ase.build import bulk, make_supercell
+from ase.optimize import BFGS
 
 hdnnpy=hdnnpy()
 hdnnpy.set_label('./Crystal')
 a1 = bulk('Si', 'diamond', a=3.6)
-a1.set_calculator(hdnnpy)
-e=a1.get_potential_energy()
+super=make_supercell(a1,[[2,1,1],[1,2,1],[1,1,2]])
+super.set_calculator(hdnnpy)
+e=super.get_potential_energy()
 print(e)
-f=a1.get_forces()
+f=super.get_forces()
 print(f)
-
-
+dyn=BFGS(super)
+dyn.run(fmax=0.05)
